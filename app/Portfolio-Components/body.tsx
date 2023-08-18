25e9de74e4d6b9820e75325b41de2a3a1164aab3
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-08-18 15:52:30 -0400

    Added Landing Page

---
+++ app/Portfolio-Components/body.tsx
@@ -0,0 +1,11 @@
+
+export default function Body() {
+    return(
+        <main className="bg-slate-700 h-screen w-auto">
+            <div className="flex justify-center text-2xl pt-39">
+                <h1>Christopher Nielson's Portfolio Coming Soon</h1>
+            </div>
+        </main>
+    )
+
+}
