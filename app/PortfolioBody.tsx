895b3563b8dbf7a24b77ab37f6bfb9bb1192c9d9
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-08-18 16:14:23 -0400

    Update

--- app/PortfolioBody.tsx
+++ app/PortfolioBody.tsx
@@ -3,7 +3,7 @@ export default function Body() {
     return(
         <main className="bg-slate-700 h-screen w-auto">
             <div className="flex justify-center text-2xl pt-39">
-                <h1>Christopher Nielson's Portfolio Coming Soon</h1>
+                <h1>Christopher Nielson Portfolio Coming Soon</h1>
             </div>
         </main>
     )
