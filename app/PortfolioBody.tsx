490b82dfd0b5eb44b18e18c455c1af2687463e9b
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-07 11:05:53 -0400

    Update

--- app/PortfolioBody.tsx
+++ app/PortfolioBody.tsx
@@ -2,50 +2,56 @@
 import Link from 'next/link'
 export default function Body() {
     return(
-    <main className='bg-gray-100'>
-        <section className="py-10 md:py-16 h-screen  flex justify-center items-center">
+    <main className='bg-gray-100 '>
+        <section className="py-10 md:py-16 h-screen flex justify-center items-center">
             <div className="container max-w-screen-xl mx-auto px-4 mt-20 ">
                 <div className=" flex flex-col md:flex-row justify-left items-center md:items-start mb-5">
                 <img src="home-image.jpg" className="grayscale rounded-2xl  md:mr-12 lg:h-23 w-auto " alt="Image" />
-                <div className="mr- ">
-                    <h6 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-5">Christopher Nielson</h6>
-                    <h1 className="font-normal text-gray-900 text-4xl md:text-5xl leading-none mb-5">Computer Science Student</h1>
-                    <p className="font-normal text-gray-600 text-md md:text-xl mb-7">Driven by a deep passion for financial technology and proficient in Python, Java, and C++, my career objective is to secure an internship or career opportunity where I can apply my technical skills and knowledge. With a strong commitment to professional growth, I aim to contribute to the success of an innovative organization in the fintech industry. I am a dedicated and proactive individual, constantly seeking opportunities to expand my expertise and stay at the forefront of industry trends.</p>
-                    
-                    <ul className="flex space-x-5 sm:mt-0 mb-9">
-                            <li>
-                                <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-auto h-10" fill="currentColor" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>     
-                                </a>
-                            </li>                       
-                            <li>
-                                <a href="https://www.linkedin.com/in/christopherjnielson/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-auto h-10" fill="currentColor"  viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>                                
-                                </a>
-                            </li>
+                    <div className="mr- ">
+                        <h6 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-5">Christopher Nielson</h6>
+                        <h1 className="font-normal text-gray-900 text-4xl md:text-5xl leading-none mb-5">Computer Science Student</h1>
+                        <p className="font-normal text-gray-600 text-md md:text-xl mb-7">Driven by a deep passion for financial technology and proficient in Python, Java, and C++, my career objective is to secure an internship or career opportunity where I can apply my technical skills and knowledge. With a strong commitment to professional growth, I aim to contribute to the success of an innovative organization in the fintech industry. I am a dedicated and proactive individual, constantly seeking opportunities to expand my expertise and stay at the forefront of industry trends.</p>
+                        
+                        <ul className="flex space-x-5 sm:mt-0 mb-9">
+                                <li>
+                                    <a href="https://github.com/chrisjnielson44" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-auto h-10" fill="currentColor" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>     
+                                    </a>
+                                </li>                       
+                                <li>
+                                    <a href="https://www.linkedin.com/in/christopherjnielson/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-auto h-10" fill="currentColor"  viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>                                
+                                    </a>
+                                </li>
+                                
+                                <li>
+                                    <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900">
+                                        <svg className="w-auto h-10" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                                    </a>
+                                </li>
+                                {/* <li>
+                                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
+                                    </a>
+                                </li> */}
                             
-                            <li>
-                                <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900">
-                                    <svg className="w-auto h-10" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
-                                </a>
-                            </li>
-                            {/* <li>
-                                <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
-                                </a>
-                            </li> */}
-                           
-                        </ul>
-                    <a href="https://chris-n.s3.us-east-2.amazonaws.com/Christopher-Nielson-Resume.pdf" target="_blank" rel="noopener noreferrer">
-                    <button className="px-7 py-3 md:px-9 md:py-4 bg-white w-full font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500">Get my CV</button>
+                            </ul>
+                        <a href="https://chris-n.s3.us-east-2.amazonaws.com/Christopher-Nielson-Resume.pdf" target="_blank" rel="noopener noreferrer">
+                        <button className="px-7 py-3 md:px-9 md:py-4 bg-white w-full font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500 shadow-xl">Get my CV</button>
+                        </a>
+                    </div>
+                </div>
+                {/* <div className='flex justify-center items-center'>
+                    <a href="">
+                    <svg className='h-10 w-10 stroke-black stroke-3' xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z"/></svg>
                     </a>
-                </div>
-                </div>
+                </div> */}
+                
             </div>
         </section>
        
         
-<section className="py-10 md:py-16 h-screen flex justify-center items-center">
+<section className="py-10 md:py-16 h-screen flex justify-center items-center snap-center">
 
 <div className="container max-w-screen-xl mx-auto px-4">
 
@@ -64,11 +70,11 @@ export default function Body() {
             <h4 className="font-bold text-gray-700 text-lg mt-5 mb-2 hover:underline underline-offset-4">International Fan Media</h4> 
             </a>
             <h3 className="font-medium text-gray-700 text-sm mb-5">Developing a dynamic, responsive multi-page website for an emerging Sports Media Company. The platform not only offers an immersive experience for Spanish soccer enthusiasts but also showcases the latest news, articles, and media in an intuitive layout.</h3>
-            <p className="text-gray-700 text-sm">
-                <span className="bg-gray-200 text-gray-700 px-2 py-1  rounded-lg">
+            <p className="text-gray-700 text-sm ">
+                <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-lg ">
                     Next.js
                 </span>
-                <span className="bg-gray-200 text-gray-700 px-2 py-1 mx-1 rounded-lg">
+                <span className="bg-gray-200 text-gray-700 px-2 py-1 mx-1 rounded-lg ">
                     TypeScript
                 </span>
                 <span className="bg-gray-200 text-gray-700 px-2 py-1 mx-1 rounded-lg">
@@ -84,9 +90,9 @@ export default function Body() {
                         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z"/></svg>                </a>
                 </li>
                 <li>
-                                    <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>     
-                                    </a>
+                        <a href="https://github.com/chrisjnielson44/internationalfanmedia-website" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>     
+                        </a>
                 </li> 
             </ul>
         </div>
@@ -167,7 +173,7 @@ export default function Body() {
         </div>
     </div>
     <div className='flex justify-center mt-4'>
-        <Link href="/ReMin/Videos#uploads" className="border-2 border-gray-400 bg-white text-gray-500 w-screen text-center text-lg py-2 rounded-full mt-4 hover:bg-slate-900 hover:text-white transition duration-300 ease-in-out">View More</Link>
+        <Link href="/ReMin/Videos#uploads" className="border-2 border-gray-400 bg-white text-gray-500 w-screen text-center text-lg py-2 rounded-full mt-4 hover:bg-slate-900 hover:text-white transition duration-300 ease-in-out">View More on Github </Link>
     </div>
 
 
@@ -194,7 +200,7 @@ export default function Body() {
                         <h3 className="text-sm lg:text-2xl font-bold tracking-tight text-black pb-3">
                              Florida State University
                         </h3>
-                        <span className="text-gray-900 dark:text-gray-800 text-xl">Bachelor of Science, Computer Science, Minor in Business & Mathematics</span>
+                        <span className="text-gray-900 dark:text-gray-800 text-xl ">Bachelor of Science, Computer Science, Minor in Business & Mathematics</span>
                         </div>
                     </div>
                 </div>
