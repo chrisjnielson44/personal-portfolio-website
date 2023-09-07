c09c5c91bd4a88b8d61954549d6d4d1394af0a02
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-07 03:50:11 -0400

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Body from "./PortfolioBody"
-import Footer from './Footer'
 export default function Home() {
   return (
     <div>
       <Body/>
-      {/* <Footer/> */}
     </div>
 
   )
