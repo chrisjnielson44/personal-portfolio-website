96c712ad489f7fb73ef478d080944c20ab886648
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-10 22:13:56 -0400

    Update

--- app/PortfolioBody.tsx
+++ app/PortfolioBody.tsx
@@ -33,7 +33,7 @@ export default function Body() {
 
                             </ul>
                             <a href="https://chris-n.s3.us-east-2.amazonaws.com/Christopher-Nielson-Resume.pdf" target="_blank" rel="noopener noreferrer">
-                                <button className="animate-fade-up animate-ease-linear px-7 py-3 md:px-9 md:py-4 bg-white w-full font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500 shadow-xl">Get my CV</button>
+                                <button className="animate-fade-up animate-ease-linear px-7 py-3 md:px-9 md:py-4 bg-white w-full font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-500 shadow-xl">Get my Resume</button>
                             </a>
                         </div>
                     </div>
@@ -54,7 +54,7 @@ export default function Body() {
                     <p className="font-normal text-gray-500 text-xs md:text-base mb-10"></p>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-                        <div className="bg-gray-50 px-8 py-10 rounded-lg drop-shadow-xl">
+                        <div className="bg-gray-50 px-8 py-8 rounded-lg drop-shadow-xl">
                             <img className="rounded-full border-2 shadow-lg" src="edit.png" alt="logo" height="70px" width="70px"></img>
                             <h4 className="font-bold text-gray-700 text-lg mt-5 mb-2">Personal Finance App</h4>
                             <h3 className="font-medium text-gray-700 text-sm mb-5">Coming Soon</h3>
@@ -70,15 +70,15 @@ export default function Body() {
                                 </li>
                             </ul>
                         </div>
-                        <div className="bg-gray-50 px-8 py-10 rounded-lg drop-shadow-xl">
+                        <div className="bg-gray-50 px-8 py-8 rounded-lg drop-shadow-xl">
                             <a href="https://www.internationalfansmedia.com" target="_blank" rel="noopener noreferrer">
                                 <img className="rounded-full p-2 border-2 shadow-lg" src="RMLogo.png" alt="logo" height="70px" width="70px"></img>
                             </a>
                             <a href="https://www.internationalfansmedia.com" target="_blank" rel="noopener noreferrer">
                                 <h4 className="font-bold text-gray-700 text-lg mt-5 mb-2 hover:underline underline-offset-4">International Fan Media</h4>
                             </a>
-                            <h3 className="font-medium text-gray-700 text-sm mb-8">Developing a dynamic, responsive multi-page website for an emerging Sports Media Company. The platform not only offers an immersive experience for Spanish soccer enthusiasts but also showcases the latest news, articles, and media in an intuitive layout.</h3>
-                            <p className="text-gray-700 text-sm ">
+                            <h3 className="font-medium text-gray-700 text-sm mb-5">Developing a dynamic, responsive multi-page website for an emerging Sports Media Company. The platform not only offers an immersive experience for Spanish soccer enthusiasts but also showcases the latest news, articles, and media in an intuitive layout.</h3>
+                            <p className="text-gray-700 text-sm mb-5">
                                 <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-lg ">
                                     Next.js
                                 </span>
@@ -104,16 +104,16 @@ export default function Body() {
                                 </li>
                             </ul>
                         </div>
-                        <div className="bg-gray-50 px-8 py-10 rounded-lg drop-shadow-xl">
+                        <div className="bg-gray-50 px-8 py-8 rounded-lg drop-shadow-xl">
                             <a href="https://cjnielson.com" target="_blank" rel="noopener noreferrer" className="">
                                 <img className="border-2 rounded-full grayscale shadow-lg" src="smallprof.png" alt="logo" height="70px" width="70px"></img>
                             </a>
                             <a href="https://cjnielson.com" target="_blank" rel="noopener noreferrer" className="">
                                 <h4 className="font-bold text-gray-700 text-lg mt-5 mb-2 hover:underline underline-offset-4">Personal Porfolio Website</h4>
                             </a>
-                            <h3 className="font-medium text-gray-700 text-sm mb-8">
+                            <h3 className="font-medium text-gray-700 text-sm mb-5">
                                 I have created a static personal portfolio website using Next.js. This site showcases my skills, projects, and achievements with a clean and user-friendly design. It is optimized for performance and provides a professional online presence to highlight my talents and accomplishments.</h3>
-                            <p className="text-gray-700 text-sm">
+                            <p className="text-gray-700 text-sm mb-5">
                                 <span className="bg-gray-200 text-gray-700 px-2 py-1  rounded-lg">
                                     Next.js
                                 </span>
@@ -137,15 +137,15 @@ export default function Body() {
                                 </li>
                             </ul>
                         </div>
-                        <div className="bg-gray-50 px-8 py-10 rounded-lg drop-shadow-xl">
+                        <div className="bg-gray-50 px-8 py-8 rounded-lg drop-shadow-xl">
                             <a href="https://github.com/chrisjnielson44/AI-Chatbot" target="_blank" rel="noopener noreferrer" className="">
-                                <img className="rounded-full  shadow-lg border-2 p-2" src="chatgpt.png" alt="logo" height="70px" width="70px"></img>
+                                <img className="p-3 rounded-full  shadow-lg border-2" src="chatgpt.png" alt="logo" height="70px" width="70px"></img>
                             </a>
                             <a href="https://github.com/chrisjnielson44/AI-Chatbot" target="_blank" rel="noopener noreferrer" className="">
                                 <h4 className="font-bold text-gray-700 text-lg mt-5 mb-2 hover:underline underline-offset-4">AI Chatbot</h4>
                             </a>
-                            <h3 className="font-medium text-gray-700 text-sm mb-8">AI chatbot developed using Python and OpenAI&apos;s GPT-3.5 architecture. The chatbot is designed to remember conversations with the user and learn from them, allowing for more personalized and natural interactions.</h3>
-                            <p className="text-gray-700 text-sm ">
+                            <h3 className="font-medium text-gray-700 text-sm mb-5">AI chatbot developed using Python and OpenAI&apos;s GPT-3.5 architecture. The chatbot is designed to remember conversations with the user and learn from them, allowing for more personalized and natural interactions.</h3>
+                            <p className="text-gray-700 text-sm mb-5">
                                 <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-lg">
                                     Python
                                 </span>
@@ -162,15 +162,15 @@ export default function Body() {
                                 </li>
                             </ul>
                         </div>
-                        <div className="bg-gray-50 px-8 py-10 rounded-lg drop-shadow-xl">
+                        <div className="bg-gray-50 px-8 py-8 rounded-lg drop-shadow-xl">
                             <a href="https://github.com/chrisjnielson44/weather-bot" target="_blank" rel="noopener noreferrer" className="">
-                                <img className="rounded-full  shadow-lg border-2 p-2" src="weatherbot.png" alt="logo" height="70px" width="70px"></img>
+                                <img className="p-3 rounded-full  shadow-lg border-2 " src="weatherbot.png" alt="logo" height="70px" width="70px"></img>
                             </a>
                             <a href="https://github.com/chrisjnielson44/weather-bot" target="_blank" rel="noopener noreferrer" className="">
                                 <h4 className="font-bold text-gray-700 text-lg mt-5 mb-2 hover:underline underline-offset-4">Weather Bot</h4>
                             </a>
                             <h3 className="font-medium text-gray-700 text-sm mb-5">This is a weather bot project that utilizes weatherAPI.com for real-time updates on current weather and daily forecast. The project is written in Python and includes Twitter functionality and SMS functionality for users to receive updates on the weather.</h3>
-                            <p className="text-gray-700 text-sm">
+                            <p className="text-gray-700 text-sm mb-5">
                                 <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-lg">
                                     Python
                                 </span>
@@ -184,7 +184,7 @@ export default function Body() {
                                 </li>
                             </ul>
                         </div>
-                        <div className="bg-gray-50 px-8 py-10 rounded-lg drop-shadow-xl">
+                        <div className="bg-gray-50 px-8 py-8 rounded-lg drop-shadow-xl">
                             <a href="https://github.com/chrisjnielson44/wordOfTheDay-bot" target="_blank" rel="noopener noreferrer" className="">
                                 <img className="rounded-full  shadow-lg border-2 p-2" src="wordoftheday.png" alt="logo" height="70px" width="70px"></img>
                             </a>
@@ -291,20 +291,17 @@ export default function Body() {
                 </div>
 
             </section>
-            <section className="py-5 md:py-16">
+            <section className="h-full py-5 md:py-16">
 
                 <div className="container max-w-screen-xl mx-auto px-4">
 
-                    <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Experience</h1>
+                    <h1 className="font-bold text-gray-700 text-3xl md:text-4xl mb-5 ">Experience</h1>
                     <div className="mb-8 h-1 bg-gray-700"></div>
-
-
-                    <p className="font-normal text-gray-500 text-xs md:text-base mb-20">Below is a summary of the places I studied</p>
 
                     <div className="flex flex-col lg:flex-row justify-between mb-20">
                         <div className="space-y-8 md:space-y-16 mb-16 md:mb-0">
                             <h6 className="font-medium text-gray-400 text-base uppercase">Company</h6>
-                            <p className=" font-semibold text-gray-600 text-base">Isofy <span className="font-normal text-gray-300">/ New York</span></p>
+                            <p className=" font-semibold text-gray-600 text-base">Isofy <span className="font-normal text-gray-300">/ Naples, FL</span></p>
 
 
                         </div>
@@ -312,7 +309,7 @@ export default function Body() {
                         <div className="space-y-8 md:space-y-16 mb-16 md:mb-0">
                             <h6 className="font-medium text-gray-400 text-base uppercase">Position</h6>
 
-                            <p className="font-normal text-gray-400 text-base">Junior Front-End Developer</p>
+                            <p className="font-normal text-gray-400 text-base">Software Engineer Intern</p>
 
 
                         </div>
@@ -320,7 +317,7 @@ export default function Body() {
                         <div className="space-y-8 md:space-y-16">
                             <h6 className="font-medium text-gray-400 text-base uppercase">Year</h6>
 
-                            <p className="font-normal text-gray-400 text-base">2016</p>
+                            <p className="font-normal text-gray-400 text-base">2020-2021</p>
 
 
                         </div>
@@ -332,6 +329,26 @@ export default function Body() {
                 </div>
 
             </section>
+            <footer className="p-4 mt-10 sm:p-6 ">
+                <div className="mx-auto max-w-screen-xl">
+                        
+                    <hr className="my-6 border-gray-300 sm:mx-auto  lg:my-8" />
+                    <div className="sm:flex sm:items-center sm:justify-between">
+                        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"> Christopher Nielson </span>
+                        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+                            <a href="https://github.com/chrisjnielson44" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5  " fill="currentColor" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
+                            </a>
+                            <a href="https://www.linkedin.com/in/christopherjnielson/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900 ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+                            </a>
+                            <a href="" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-900">
+                                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+        </footer>
 
         </main>
 
