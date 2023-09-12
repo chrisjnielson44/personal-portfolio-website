769a1ed233fa97a89aace6a8784d618a4185af4f
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-11 20:31:37 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Education from "./Components/Education"
 import Research from "./Components/Research"
 import Experience from "./Components/Experience"
 import Footer from "./Components/Footer"
+
 export default function Home() {
   return (
     <div className='bg-gray-100 '>
